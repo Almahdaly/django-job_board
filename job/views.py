7d6bce40473ae8edd9584316361b035566f2a2b8
Yasser <yasseralmahdaly@gmail.com>
2024-10-02 09:46:20 +0300

    add 2 template to project (job_details - job_list)

--- job/views.py
+++ job/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import job
+
 
 # Create your views here.
 def job_list(request):
-    pass
+    job_list=job.objects.all()
+    cotext={'jobs':job_list}
+    return render(request,"job/job_list.html",cotext)
 
 def job_detail(request, id):
-    pass
+    job_detail=job.objects.get(id=id)
+    cotext={'job':job_detail}
+    return render(request,"job/job_details.html",cotext)
