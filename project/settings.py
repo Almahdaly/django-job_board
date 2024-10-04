374729fba6d80a2c8f1343b0488d564a8f157cce
Yasser <yasseralmahdaly@gmail.com>
2024-10-04 22:19:47 +0300

    add email to project and use to send email

--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'contact',
     'django_bootstrap5',
     'django_filters',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -140,3 +141,12 @@ MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_USE_TLS= True
+
+EMAIL_HOST_USER='yasseralmahdaly@gmail.com'
+EMAIL_HOST_PASSWORD='rveb vuei oprm josc'
