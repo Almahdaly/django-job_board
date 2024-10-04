34ff82f59ef49a22c722b6bfdc4b27754ca0d99b
Yasser <yasseralmahdaly@gmail.com>
2024-10-04 23:00:24 +0300

    add rest api get method for job app

--- job/urls.py
+++ job/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from . import views
+from . import api
 
 app_name='job'
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('',views.job_list,name='job_list'),
     path('add',views.jop_add,name='add_job'),
     path('<str:slug>',views.job_detail,name='job_details'),
+
+    # api
+    path('api/list',api.joblistapi,name='job_list_api'),
 ]
