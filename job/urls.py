367f5caa142bcf4a8b957a5e3b1634d8d301d2bc
Yasser <yasseralmahdaly@gmail.com>
2024-10-05 07:51:18 +0300

    add put & post & delete oprations as CBV & FBV

--- job/urls.py
+++ job/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     path('<str:slug>',views.job_detail,name='job_details'),
 
     # api
-    path('api/list',api.joblistapi,name='job_list_api'),
+    path('api/jobs',api.joblistapi,name='job_list_api'),
+    path('api/jobs/<int:id>',api.job_detail_api,name='job_detail_api'),
+
+    #CBV
+    path('api/v2/jobs',api.JobListApi.as_view(),name='Job_List_Api'),
+    path('api/v2/jobs/<int:id>',api.JobDetail.as_view(),name='Job_Detail'),
+
 ]
